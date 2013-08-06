213cb50ff9eb0fba058a0727b6133646600f328c
lncubus <randirbox@gmail.com>
2013-08-07 01:23:46 +0400

    Introduced 1-2-5-10 scale control.

---
+++ Controls/ScaleControl.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SF.Controls
+{
+    partial class ScaleControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelScale = new System.Windows.Forms.Label();
+            this.buttonZoomOut = new System.Windows.Forms.Button();
+            this.buttonZoomIn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelScale
+            // 
+            this.labelScale.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelScale.Location = new System.Drawing.Point(0, 0);
+            this.labelScale.Name = "labelScale";
+            this.labelScale.Size = new System.Drawing.Size(191, 29);
+            this.labelScale.TabIndex = 5;
+            this.labelScale.Text = "0 км";
+            this.labelScale.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonZoomOut
+            // 
+            this.buttonZoomOut.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonZoomOut.BackColor = System.Drawing.Color.White;
+            this.buttonZoomOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonZoomOut.Location = new System.Drawing.Point(165, 3);
+            this.buttonZoomOut.Name = "buttonZoomOut";
+            this.buttonZoomOut.Size = new System.Drawing.Size(23, 23);
+            this.buttonZoomOut.TabIndex = 4;
+            this.buttonZoomOut.TabStop = false;
+            this.buttonZoomOut.Text = "-";
+            this.buttonZoomOut.UseVisualStyleBackColor = false;
+            this.buttonZoomOut.Click += new System.EventHandler(this.buttonZoomOut_Click);
+            // 
+            // buttonZoomIn
+            // 
+            this.buttonZoomIn.BackColor = System.Drawing.Color.White;
+            this.buttonZoomIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonZoomIn.Location = new System.Drawing.Point(3, 3);
+            this.buttonZoomIn.Name = "buttonZoomIn";
+            this.buttonZoomIn.Size = new System.Drawing.Size(23, 23);
+            this.buttonZoomIn.TabIndex = 3;
+            this.buttonZoomIn.TabStop = false;
+            this.buttonZoomIn.Text = "+";
+            this.buttonZoomIn.UseVisualStyleBackColor = false;
+            this.buttonZoomIn.Click += new System.EventHandler(this.buttonZoomIn_Click);
+            // 
+            // ScaleControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Controls.Add(this.buttonZoomOut);
+            this.Controls.Add(this.buttonZoomIn);
+            this.Controls.Add(this.labelScale);
+            this.Name = "ScaleControl";
+            this.Size = new System.Drawing.Size(191, 29);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelScale;
+        private System.Windows.Forms.Button buttonZoomOut;
+        private System.Windows.Forms.Button buttonZoomIn;
+
+    }
+}
---
+++ Controls/ScaleControl.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SF.Space;
+
+namespace SF.Controls
+{
+    public partial class ScaleControl : UserControl
+    {
+        public const double DefaultMinValue = 1000;
+        public const double DefaultMaxValue = 1000000000;
+        private int m_first = 1;
+        private int m_zeros = 7;
+        private string m_unit = "км";
+
+        public string Unit
+        {
+            get
+            {
+                return m_unit;
+            }
+            set
+            {
+                m_unit = value;
+                UpdateDigits();
+            }
+        }
+
+        public double MaxValue { get; set; }
+        public double MinValue { get; set; }
+
+        public double Value
+        {
+            get
+            {
+                return Math.Round(m_first * Math.Pow(10, m_zeros));
+            }
+            set
+            {
+                if (value  < MinValue)
+                    value = MinValue;
+                else if (value > MaxValue)
+                    value = MaxValue;
+                m_zeros = (int)Math.Truncate(Math.Log10(value));
+                double first = value / Math.Pow(10, m_zeros);
+                if (first < 1.5)
+                    m_first = 1;
+                else if (first < 3.5)
+                    m_first = 2;
+                else if (first < 7.5)
+                    m_first = 5;
+                else
+                {
+                    m_first = 1;
+                    m_zeros++;
+                }
+            }
+        }
+
+        public ScaleControl()
+        {
+            InitializeComponent();
+            MinValue = DefaultMinValue;
+            MaxValue = DefaultMaxValue;
+            UpdateDigits();
+        }
+
+        private void buttonZoomIn_Click(object sender, EventArgs e)
+        {
+            Value /= 3.3;
+        }
+
+        private void buttonZoomOut_Click(object sender, EventArgs e)
+        {
+            Value *= 3.3;
+        }
+
+        private void UpdateDigits()
+        {
+            labelScale.Text = MathUtils.NumberToText(Value, Unit);
+        }
+    }
+}
