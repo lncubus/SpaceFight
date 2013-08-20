89416b99642a0087beab1ad38e6d50f999b7c98e
lncubus <randirbox@gmail.com>
2013-08-20 10:56:44 +0400

    Added anoher particle. Probably need Particle interface

---
+++ Space/Star.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SF.Space
+{
+    public class Star
+    {
+        public Vector S;
+        public Vector V;
+
+    }
+}
