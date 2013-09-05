44d4116d82b64e9eb5c6d38cf9b97d91fb63d4f6
lncubus <randirbox@gmail.com>
2013-09-05 13:52:07 +0400

    Fixed State saving

--- ServerLibrary/Helm.cs
+++ ServerLibrary/Helm.cs
@@ -70,6 +70,7 @@ namespace SF.ServerLibrary
                 Dynamics = shipDynamics,
                 Missile = missileClass,
                 Missiles = that.Missiles,
+                State =  that.State,
             };
         }
     }
