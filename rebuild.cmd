4f94ca0fb74e78cf2160bd9b0c6b2d10df8b94a6
lncubus <randirbox@gmail.com>
2013-09-11 15:05:30 +0400

    fixed rebuild to work on x86/x64 systems

--- rebuild.cmd
+++ rebuild.cmd
@@ -1,13 +1,17 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv" /rebuild Debug %CD%\sf.sln
 set bin=..\SpaceFight_bin
+set devenv="C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com"
+if not exist %devenv% {
+set devenv="C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com"
+}
+%devenv% /rebuild Debug %CD%\sf.sln
 
 echo y | rmdir /S /Q %bin%
 
 mkdir %bin%\server
 mkdir %bin%\client
 
-copy /Y Gunner\bin\Debug\*.* %bin%\client
-copy /Y Pilot\bin\Debug\*.* %bin%\client
-copy /Y Server\bin\Debug\*.* %bin%\server
-echo y | del %bin%\client\*.vshost.*
-echo y | del %bin%\server\*.vshost.*
+copy /Y Gunner\bin\Debug\*.* %bin%\client > nul
+copy /Y Pilot\bin\Debug\*.* %bin%\client > nul
+copy /Y Server\bin\Debug\*.* %bin%\server > nul
+echo y | del %bin%\client\*.vshost.* > nul
+echo y | del %bin%\server\*.vshost.* > nul
