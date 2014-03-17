e999bb9e49b424f47dc354c07701bb083facc2fe
lncubus <randirbox@gmail.com>
2014-03-17 12:26:45 +0400

    deleted deprecated files

--- Space/deprecated/IMissile.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SF.Space
-{
-    public interface IMissile : IParticle
-    {
-        MissileClass Class { get; }
-        string ClassName { get; }
-        bool IsDead { get; }
-    }
-}
